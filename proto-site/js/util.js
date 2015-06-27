c60c2d0a0ea1bfedbc38b6db3565e3adf2d15068
dbavedb <dbavedb@shaw.ca>
2015-06-27 14:31:35 -0700

    Modifications necessary to get 404s working for this version of the site:  http://www.bcitcomp.ca/students/dklassen4/hw9/proto-site/index.html

---
+++ proto-site/js/util.js
@@ -0,0 +1,11 @@
+/*
+	- Student No: A00026160
+	- Project: Course website for BCIT COMP 1950 
+	- Work:	Script to provide the actual URL path prompting a 404.
+	- File: 404/js/util.js
+	-
+*/
+
+window.onload=function() {
+	document.getElementById('filepath').innerHTML = document.URL;
+}
